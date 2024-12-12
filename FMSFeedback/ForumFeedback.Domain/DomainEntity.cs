7dbf7ed476cb1a75f1b62ceb5cbf9bdedf4d2c30
Gustne <bojesondertoftpedersen@gmail.com>
2024-12-12 23:57:16 +0100

    Update DomainEntity.cs

--- FMSFeedback/ForumFeedback.Domain/DomainEntity.cs
+++ FMSFeedback/ForumFeedback.Domain/DomainEntity.cs
@@ -5,6 +5,6 @@ namespace ForumFeedback.Domain;
 public abstract class DomainEntity
 {
     public int Id { get; protected set; }
-    [Timestamp]
+    
     public byte[] RowVersion { get; protected set; }
 }
